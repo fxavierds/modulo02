9093ef05d5460772514cf69460ed79c3ca4f4a5e
fxavierds <fxavierds@yahoo.com.br>
2021-03-11 23:11:05 -0300

    Sequelize migrations

---
+++ src/app/models/User.js
@@ -0,0 +1,17 @@
+import Model, { Sequelize } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        nome: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      { sequelize }
+    );
+  }
+}
+
+export default new User();
---
+++ src/database/migrations/20210312003414-create-user.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncremente: true,
+        primaryKey: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      create_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  },
+};
