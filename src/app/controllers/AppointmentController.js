e7dbfc304d203580b54a7ccf9999bf5f1be5d903
Xavier <fxavierds@yahoo.com.br>
2021-07-09 22:16:51 -0300

    Avatar

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,38 @@
-import Appointment from "../models/Appointment";
+import * as Yup from 'yup';
+import {startOfHour, parseISO} from 'date-fns';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
 
-class AppointmentController{
-  async store(req, res){
-    return res.json();
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required,
+      date: Yup.date().required,
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validação falhou' });
+    }
+
+    const { provider_id, date } = req.body;
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Você só pode marcar agendamento como provider' });
+    }
+    const hourStart = startOfHour(parseISO(date));
+
+    const appointment = await Appointment.create({
+      user_id: res.user_id,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,6 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
-import route from 'color-convert/route';
 
 const routes = new Router();
 const upload = multer(multerConfig);
